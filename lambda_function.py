93d450960c524a556f3fc53f797dec11721cad6e
Jake Richard <jake.richard126@gmail.com>
2023-03-31 15:07:58 -0400

    Check for issue creation

--- lambda_function.py
+++ lambda_function.py
@@ -5,14 +5,15 @@ import requests
 SLACK_URL = os.environ.get("SLACK_URL", "")
 
 def lambda_handler(event, context):
-    # Construct the payload
-    payload = f"{{\'text\':\'Issue Created: {event['issue']['html_url']}\'}}"
+    if event['action'] == 'opened':
+        # Construct the payload
+        payload = f"{{\'text\':\'Issue Created: {event['issue']['html_url']}\'}}"
 
-    # Send the payload
-    r = requests.post(SLACK_URL, data=payload)
+        # Send the payload
+        r = requests.post(SLACK_URL, data=payload)
 
-    # Return the response
-    return {
-        "statusCode": r.status_code,
-        "body": r.text
-    }
+        # Return the response
+        return {
+            "statusCode": r.status_code,
+            "body": r.text
+        }
