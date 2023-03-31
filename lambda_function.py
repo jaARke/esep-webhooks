9bb72c2b06f802de596756b3c696c2d47085a0d0
Jake Richard <jake.richard126@gmail.com>
2023-03-31 15:04:46 -0400

    Updates to lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -5,13 +5,8 @@ import requests
 SLACK_URL = os.environ.get("SLACK_URL", "")
 
 def lambda_handler(event, context):
-    # Parse the event
-    body_str = event.get("body", "{}")
-    body_str = body_str if body_str else "{}"
-    body_obj = json.loads(body_str)
-    
     # Construct the payload
-    payload = f"{{\'text\':\'Issue Created: {body_obj.get('issue', {}).get('url', '')}\'}}"
+    payload = f"{{\'text\':\'Issue Created: {event['issue']['html_url']}\'}}"
 
     # Send the payload
     r = requests.post(SLACK_URL, data=payload)
